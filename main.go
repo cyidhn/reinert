c4641f474f681240c4194c3429f82f01a7cfc046
vincent <vincenttran94@hotmail.fr>
2022-04-25 17:05:24 +0200

    Import de fichier

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
 
 func main() {
 	// 1. Fonction pour importer le document en format iramuteq
+	file, err := ioutil.ReadFile("./corpus/all.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(string(file))
 	// 2. Traitement du texte importe
+
 	// 3. Algo CHD
 	// 4. Retourne les resultats en JSON
-	fmt.Println("Hello")
 }
