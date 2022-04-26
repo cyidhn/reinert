c98a42d97da61e6247edca2aae8c13b3a7168e59
vincent <vincenttran94@hotmail.fr>
2022-04-26 12:10:21 +0200

    test

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
+	"os"
 )
 
 func main() {
 	// 1. Fonction pour importer le document en format iramuteq
 	file, err := ioutil.ReadFile("./corpus/all.txt")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	fmt.Print(string(file))
 	// 2. Traitement du texte importe Traitement de texte par Rainette
-	////
-	///
-	///
+	output := bytes.Replace(file, []byte("*"), []byte(" "), -1)
+
+	if err = ioutil.WriteFile("./corpus/new_file.txt", output, 0666); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Print(string(output))
 	// 4. Retourne les resultats en JSON
 }
