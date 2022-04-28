c3478a94d8058b059a968b28ad42fb635c0d05d1
vincent <vincenttran94@hotmail.fr>
2022-04-28 12:26:08 +0200

    commit

--- main.go
+++ main.go
@@ -14,15 +14,6 @@ func isMn(r rune) bool {
 	return unicode.Is(unicode.Mn, r) // Mn: nonspacing marks
  }
 
-func preprocessing(string text)
-	list_reg := `(\d+)|(http\S+)|(www\S+)|(@mention)|(&[a-z])`
-	reg := regexp.MustCompile(list_reg)
-	res := reg.ReplaceAllString(text, "")
-
-	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
-	result, _, _ := transform.String(t, text)
- 	return text
-
 func main() {
 	// 1. Fonction pour importer le document en format iramuteq
 	file, err := ioutil.ReadFile("./corpus/new_file.txt")
@@ -33,8 +24,14 @@ func main() {
 	}
 
 	// 1. Traitement de texte pour les tweets
-	
+	list_reg := `(\d+)|(http\S+)|(www\S+)|(@mention)|(&[a-z])`
+	reg := regexp.MustCompile(list_reg) // Test pour les ponctuations
+	res := reg.ReplaceAllString(string(file), "")
+
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
+   	result, _, _ := transform.String(t, string(file))
 	// 3. Algo de CHD
 	// 4. Retourne les resultats en JSON
-	fmt.Println(preprocessing(file))
+	fmt.Println(res)
+	fmt.Println(result)
 }
