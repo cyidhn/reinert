25cf4b5173fa7ed77752d80b74eea7847f2980ab
vincent <vincenttran94@hotmail.fr>
2022-04-28 10:07:45 +0200

    commit

--- test.go
+++ test.go
@@ -1,13 +1,13 @@
-package main
-
 import (
    "fmt"
    "regexp"
 )
 
-func main() {
+func test() {
+
    s := "Abraham Lincoln 43 4 3 65 http:www.google.fr"
    reg := regexp.MustCompile(`http\S+`)
    res := reg.ReplaceAllString(s, "${1}")
    fmt.Println(res) // Abraham Lincoln
+   return
 }
