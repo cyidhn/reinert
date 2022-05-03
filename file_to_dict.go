7bbec1b21c9627a7e7ca3e49498afdec911f95d3
vincent <vincenttran94@hotmail.fr>
2022-05-03 09:54:27 +0200

    fonction file to dict

---
+++ file_to_dict.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CSVToMap(reader io.Reader) []map[string]string {
+	r := csv.NewReader(reader)
+	rows := []map[string]string{}
+	var header []string
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if header == nil {
+			header = record
+		} else {
+			dict := map[string]string{}
+			for i := range header {
+				dict[header[i]] = record[i]
+			}
+			rows = append(rows, dict)
+		}
+	}
+
+	return rows
+
+func main() {
+
+}
