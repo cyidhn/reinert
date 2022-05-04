c2e116b1031f73ea1b2f8427f02dbd7ae1fb15c3
vincent <vincenttran94@hotmail.fr>
2022-05-04 16:21:40 +0200

    Remove l'ancien script pour la conversion CSV vers dictionnaire car le format n'est pas bonne + amélioration du programme princiaple

---
+++ csv_to_dict_records.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+var dict []map[string]string
+
+func csv_to_dict() []map[string]string {
+	csvFile, err := os.Open("./corpus/Lexique383.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
+
+	csvlines := csv.NewReader(csvFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rows := []map[string]string{}
+	var header []string
+
+	for {
+		record, err := csvlines.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(header) == 0 {
+			header = record
+		} else {
+			dict := map[string]string{}
+			for i := range header {
+				dict[header[i]] = record[i]
+			}
+			rows = append(rows, dict)
+		}
+	}
+	//fmt.Println(rows)
+	return rows
+}
