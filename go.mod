9fa413892c84bbb229d5f6a9b4afbcc92f71729f
vincent <vincenttran94@hotmail.fr>
2022-05-05 11:25:28 +0200

    Création fonction preprocess et la fonction lematize marche mais prend environ 15-20 min à exécuter

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module reinert
 
 go 1.18
 
-require golang.org/x/text v0.3.7
+require (
+	github.com/bbalet/stopwords v1.0.0
+	golang.org/x/text v0.3.7
+)
--- main.go
+++ main.go
@@ -8,15 +8,28 @@ import (
 	"strings"
 	"unicode"
 
+	"github.com/bbalet/stopwords"
+
 	"golang.org/x/text/transform"
 	"golang.org/x/text/unicode/norm"
 )
+
+var dict []map[string]string
 
 func isMn(r rune) bool {
 	return unicode.Is(unicode.Mn, r) // Mn: nonspacing marks
 }
 
-var dict []map[string]string
+func clean_words(text string) {
+	var tab []string
+	liste_terme := []string{"ve", "n", "s", "d", "l", "j", "y", "c", "e", "m", "h", "quelqu", "cht", "lr", "oas", "qu", "ll", "yu", "an", "g", "TRUE", "jadot", "avectaubira", "zemmourcroissance", "zemmourlille", "cdanslair", "taubirasorbonne", "emmanuel", "bfmpolitique", "aujourd", "macron"}
+	result := strings.Fields(text)
+	for word := range result {
+		if result[word] != liste_terme[word] {
+			tab = append(tab)
+		}
+	}
+}
 
 func lematize(text string) string {
 	var tab []string
@@ -35,25 +48,28 @@ func lematize(text string) string {
 	return strings.Join(tab, " ")
 }
 
+// Preprocessing du text
+func preprocess(text string) string {
+	lower := strings.ToLower(text)
+	list_reg := `(\d+)|(http\S+)|(www\S+)|(@mention)|(&[a-z])|([.,'’”\/#?!$%\^&\*;:+{}=\-_~()«»])|([\x{1F600}-\x{1F6FF}|[\x{2600}-\x{26FF}])`
+	reg := regexp.MustCompile(list_reg)                                  // Test pour les ponctuations
+	res := reg.ReplaceAllString(lower, "")                               //Résultat pour Regex
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC) //Enlever UNIQUEMENT LES ACCENTS DU TEXTE
+	result, _, _ := transform.String(t, res)                             //Résultat pour remove accents
+	delete_words := stopwords.CleanString(result, "fr", true)            //Stopwords
+	//lematizer := lematize(string(delete_words))
+	//fmt.Println(lematizer)
+	return delete_words
+}
+
 func main() {
 	// 1. Fonction pour importer le document en format iramuteq
 	file, err := ioutil.ReadFile("./corpus/clean_file.txt") //Permet de lire tout l'intégralité du texte
-
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	// 1. Traitement de texte pour les tweets
-	min := strings.ToLower(string(file))
-	list_reg := `(\d+)|(http\S+)|(www\S+)|(@mention)|(&[a-z])|([.,'’”\/#?!$%\^&\*;:+{}=\-_~()«»])|([\x{1F600}-\x{1F6FF}|[\x{2600}-\x{26FF}])`
-	reg := regexp.MustCompile(list_reg)                                  // Test pour les ponctuations
-	res := reg.ReplaceAllString(string(min), "")                         //Résultat pour Regex
-	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC) //Enlever UNIQUEMENT LES ACCENTS DU TEXTE
-	result, _, _ := transform.String(t, string(res))                     //Résultat pour remove accents
-	lematizer := lematize(string(result))
-	fmt.Println(lematizer)
-	//fmt.Println(result)
+	fmt.Println(preprocess(string(file)))
 	// 3. Algo de CHD
 	// 4. Retourne les resultats en JSON
 }
